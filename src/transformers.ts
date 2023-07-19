0234a7391abd2f2042b264af8d4ba78f51a81ba1
Ni55aN <huccah@gmail.com>
2023-07-19 15:17:40 +0300

    docs: add typedoc

--- src/transformers.ts
+++ src/transformers.ts
@@ -1,5 +1,15 @@
+/**
+ * Built-in transformers, defining the auxiliary points for the path.
+ * @module
+ */
+
 import { Position, Transformer } from './types'
 
+/**
+ * Linear transformer. Returns the same points.
+ * @returns Two points
+ * @throws Error if number of points is not equal to 2
+ */
 export const linear = (): Transformer => {
   return (points) => {
     if (points.length !== 2) throw new Error('number of points should be equal to 2')
@@ -9,7 +19,16 @@ export const linear = (): Transformer => {
   }
 }
 
-export const classic = ({ vertical = false, curvature = 0.3 }): Transformer => {
+/**
+ * Classic transformer. Returns four points: start, auxiliary start, auxiliary end, end.
+ * @param options Options
+ * @param options.vertical If true, the auxiliary points will be placed vertically. Default: `false`
+ * @param options.curvature The curvature of the path. Default: `0.3`
+ * @returns Four points
+ */
+export const classic = (options: { vertical?: boolean, curvature?: number }): Transformer => {
+  const { vertical = false, curvature = 0.3 } = options
+
   function add(a: Position, b: Position) {
     return { x: a.x + b.x, y: a.y + b.y }
   }
